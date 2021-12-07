622598e0a5c0640a1e66b6f4a0a0756d6fd6d222
sedattcann <35sedatcan@gmail.com>
2021-12-07 16:45:39 +0300

    Initial commit

---
+++ src/test/java/RestAssuredTest.java
@@ -0,0 +1,133 @@
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+import java.util.List;
+import static io.restassured.RestAssured.*;
+public class RestAssuredTest {
+
+
+    @Test
+    public void simpleTest() {
+        baseURI="http://testproxy.ng112.gov.tr";
+        Response response=given()
+                .log().all()
+                .port(7777)
+                .header("accept-encoding", "gzip")
+                .header("ilKod","14")
+                .header("header","2")
+                .header("ip","10.6.6.45")
+                .header("tc","55588096488")
+                .get("/AcilNG112Middleware-war/webresources/ng112dao/createIncident");
+        response.prettyPrint();
+        System.out.println(response.statusCode());
+        List res = response.jsonPath().get("value");
+        System.out.println("vaka numarası : "+res.get(0));
+        if (response.statusCode()==200){
+            baseURI="http://testproxy.ng112.gov.tr";
+        Response response1=given()
+                .log().all()
+                .port(7777)
+                .header("accept-encoding", "gzip")
+                .contentType("application/json")
+                .accept(ContentType.JSON)
+                .header("ilKod","14")
+                .header("header","2")
+                .header("ip","10.6.6.45")
+                .header("tc","55588096488")
+                .body("{\n" +
+                        "  \"id\": "+res.get(0)+",\n" +
+                        "  \"lastModificationDate\": \"2021-12-07 10:15:00.505\",\n" +
+                        "  \"cityCode\": 14,\n" +
+                        "  \"isAssigned\": 1,\n" +
+                        "  \"closable\": false,\n" +
+                        "  \"status\": 1,\n" +
+                        "  \"priority\": 0,\n" +
+                        "  \"isTrue\": 1,\n" +
+                        "  \"isUrban\": 0,\n" +
+                        "  \"firstPublishedDate\": \"2021-12-07 10:15:00.505\",\n" +
+                        "  \"kurumList\": [\n" +
+                        "    {\n" +
+                        "      \"id\": 2,\n" +
+                        "      \"processOption\": 1,\n" +
+                        "      \"subInstutitionList\": [],\n" +
+                        "      \"olayTuruList\": [\n" +
+                        "        {\n" +
+                        "          \"id\": 1,\n" +
+                        "          \"agencyId\": 2,\n" +
+                        "          \"parenttypeId\": 0,\n" +
+                        "          \"processOption\": 1,\n" +
+                        "          \"closeState\": 0\n" +
+                        "        }\n" +
+                        "      ],\n" +
+                        "      \"assignedDate\": \"2021-12-07 10:15:00.505\",\n" +
+                        "      \"status\": 1,\n" +
+                        "      \"identityNumber\": \"11367942902\",\n" +
+                        "      \"crudState4Kurum\": 1\n" +
+                        "    }\n" +
+                        "  ],\n" +
+                        "  \"crudState4KurumOlayTuru\": 1,\n" +
+                        "  \"cagriBilgileriList\": [],\n" +
+                        "  \"addressList\": [\n" +
+                        "    {\n" +
+                        "      \"cityCode\": 14,\n" +
+                        "      \"cityName\": \"BOLU\",\n" +
+                        "      \"institutionId\": 2,\n" +
+                        "      \"processOption\": 1,\n" +
+                        "      \"coordinateSystem\": {\n" +
+                        "        \"id\": 1\n" +
+                        "      },\n" +
+                        "      \"addressType\": 1\n" +
+                        "    }\n" +
+                        "  ],\n" +
+                        "  \"actionTypeList\": [\n" +
+                        "    {\n" +
+                        "      \"id\": 2,\n" +
+                        "      \"typeName\": \"KURUMA_ATAMA_YAPILDI\",\n" +
+                        "      \"typeDescription\": \"Kuruma Atama Yapıldı.( SAĞLIK)\"\n" +
+                        "    },\n" +
+                        "    {\n" +
+                        "      \"id\": 116,\n" +
+                        "      \"typeName\": \"OLAY_TURU_GIRILDI\",\n" +
+                        "      \"typeDescription\": \"OLAY_TURU_GIRILDI ( MEDİKAL )\"\n" +
+                        "    },\n" +
+                        "    {\n" +
+                        "      \"id\": 1,\n" +
+                        "      \"typeName\": \"YENI_VAKA_OLUSTURULDU\",\n" +
+                        "      \"typeDescription\": \"Yeni Vaka Oluşturuldu.\",\n" +
+                        "      \"actionTimeInMiliseconds\": 1638794723301\n" +
+                        "    },\n" +
+                        "    {\n" +
+                        "      \"id\": 55,\n" +
+                        "      \"typeName\": \"VAKA_YAYINLANDI\",\n" +
+                        "      \"typeDescription\": \"Vaka Yayınlandı.\"\n" +
+                        "    }\n" +
+                        "  ],\n" +
+                        "  \"description\": \" Kuruma Atama Yapıldı.( SAĞLIK) :( MEDİKAL )  Yeni Vaka Oluşturuldu.:  Vaka Yayınlandı.: \",\n" +
+                        "  \"ip\": \"10.6.6.46\",\n" +
+                        "  \"roleName\": \"SAGLIK_KURUM_YONETICISI\",\n" +
+                        "  \"identityNumber\": \"11367942902\",\n" +
+                        "  \"updateIsUrbanOnAsos\": false,\n" +
+                        "  \"updateOlayTuruOnAsos\": false,\n" +
+                        "  \"updateInstitutionAssignedDateOnAsos\": false,\n" +
+                        "  \"smsIncidents\": [],\n" +
+                        "  \"enabizIncidents\": [],\n" +
+                        "  \"eventTypeId\": 0,\n" +
+                        "  \"ids\": []\n" +
+                        "}")
+                .post("/AcilNG112Middleware-war/webresources/ng112dao/incident/update");
+
+            System.out.println("Status Code" + response1.statusCode());
+            response1.prettyPrint();
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
